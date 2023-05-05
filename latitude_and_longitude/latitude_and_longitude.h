f93404a845b63fca5be9c00e868ab165923df741
AntiHexCode <>
2023-05-05 03:48:55 +0200

    Added the latitude_and_longitude files

---
+++ latitude_and_longitude/latitude_and_longitude.h
@@ -0,0 +1,14 @@
+/*
+* Author: AntiHexCode
+* Header Files, Libraries and Prototypes for Extracting the latitude and longitude from NMEA
+*/
+
+// Header files
+#include "UART.h"
+
+// Libraries
+#include <stdint.h>
+#include <stdio.h>
+
+// Prototypes
+void latitude_and_longitude (double *latitude, double *longitude);
