f09bdf1f675bf4bfbfc001281cddc41a896fe40f
AntiHexCode <131706131+AntiHexCode@users.noreply.github.com>
2023-05-13 19:42:25 +0300

    Updated latitude_and_longitude.c

--- latitude_and_longitude/latitude_and_longitude.c
+++ latitude_and_longitude/latitude_and_longitude.c
@@ -12,16 +12,17 @@ void latitude_and_longitude (double *latitude, double *longitude)
   char GPRMC[78];
   char latitude_array[12];
   char longitude_array[13];
-  int undifined_location = 1; // undifined till we get the valid GPRMC and extract the latitude and longitude
+  int undefined_location = 1; // undifined till we get the valid GPRMC and extract the latitude and longitude
+  int i = 0; // counter for all the for loops
 
   // Loop till you find the valid GPRMC and extract the latitude and longitude
-  while (undifined_location == 1){
+  while (undefined_location == 1){
 
     // Waiting until it is the start of a NMEA line
     while (uart_recieve() != '$');
 
     // Getting the whole NMEA line of characters
-    for (int i = 0; i < 78; i++)
+    for (i = 0; i < 78; i++)
       GPRMC[i] = uart_recieve();
 
     // Checking if its GPRMC or not, and if it's valid or not
@@ -29,17 +30,17 @@ void latitude_and_longitude (double *latitude, double *longitude)
       continue;
 
     // Extracting the latitude and longitude
-    for (int i = 18; i < 30; i++)
+    for (i = 18; i < 30; i++)
       latitude_array[i-18] = GPRMC [i];
 
-    for (int i = 33; i < 46; i++)
+    for (i = 33; i < 46; i++)
       longitude_array[i-33] = GPRMC [i];
 
     char latitude_direction = GPRMC [31];
     char longitude_direction = GPRMC [47];
 
     // Found the location's latitude and longitude
-    undifined_location = 0;
+    undefined_location = 0;
 
     // Converting types from char array to double
     sscanf(latitude_array, "%lf", &(*latitude));
